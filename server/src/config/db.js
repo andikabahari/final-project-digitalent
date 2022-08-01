83c1e615f96b3c06cc86a3a0c44cd7552cf3e2d9
Andika Bahari <andikabahari48@gmail.com>
2022-08-01 21:37:11 +0700

    Add server

---
+++ server/src/config/db.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Connection URL
+  url: "mongodb://127.0.0.1:27017/mern-jwt-auth",
+
+  // Options
+  options: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+};
