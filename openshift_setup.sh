91a1b9b8211577daaf0b06e3cb872e81b8b665a3
Andika Bahari <andikabahari48@gmail.com>
2022-08-03 22:07:37 +0700

    Use service route

--- openshift_setup.sh
+++ openshift_setup.sh
@@ -26,23 +26,31 @@ MONGO_IP=$(oc get svc mongo-service -o json | jq -r '.spec.clusterIP')
 MONGO_PORT=$(oc get svc mongo-service -o json | jq -r '.spec.ports[0].port')
 MONGO_DB=mern
 
+sleep 30
+
 # Create a new user for "mern" database.
 oc exec \
     $(oc get pod -l app=mongo -o name) -- /bin/bash -c \
     "mongo admin -u root -p pass --eval \"db.getSiblingDB('mern').createUser({user: 'user', pwd: 'pass', roles: ['readWrite']})\""
 
-# Set environment variables in nodejs-deployment.yaml,
-# create nodejs resources, and
-# retrieve nodejs service's ClusterIP and port.
+# Set environment variables in nodejs-deployment.yaml and
+# create nodejs resources.
 sed -i "s/\$MONGO_IP/$MONGO_IP/" ./k8s/nodejs-deployment.yaml
 sed -i "s/\$MONGO_PORT/$MONGO_PORT/" ./k8s/nodejs-deployment.yaml
 sed -i "s/\$MONGO_DB/$MONGO_DB/" ./k8s/nodejs-deployment.yaml
 oc apply -f ./k8s/nodejs-secret.yaml
 oc apply -f ./k8s/nodejs-deployment.yaml
 oc apply -f ./k8s/nodejs-service.yaml
-NODEJS_IP=$(oc get svc nodejs-service -o json | jq -r '.spec.clusterIP')
-NODEJS_PORT=$(oc get svc nodejs-service -o json | jq -r '.spec.ports[0].port')
-NGINX_API="http://$NODEJS_IP:$NODEJS_PORT"
+
+# Access nodejs service using ClusterIP.
+# NODEJS_IP=$(oc get svc nodejs-service -o json | jq -r '.spec.clusterIP')
+# NODEJS_PORT=$(oc get svc nodejs-service -o json | jq -r '.spec.ports[0].port')
+# NGINX_API="http:\/\/$NODEJS_IP:$NODEJS_PORT"
+
+# Access nodejs service using route. 
+oc expose svc/nodejs-service
+NODEJS_HOST=$(oc get route nodejs-service -o json | jq -r '.spec.host')
+NGINX_API="http://$NODEJS_HOST"
 
 # Set environment variables in nginx-deployment.yaml and
 # create nginx resources.
